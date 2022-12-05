57187d1849a10f4e0a8a6171f8459bb47de80053
shreyaaa-s <ss16396@nyu.edu>
2022-12-05 14:33:11 -0500

    First Scene - somewhat done?
    I am able to get to the first dialogue scene, but I'm getting errors in the p5.js sketch that I don't know how to address (because I did not write that code). It has to do with using and removing the buttons. I'm also unsure of how to get to the next dialogue screen? The screen is not changing. This might be because I started everything in the setup() function and not the draw loop, so everything's only running once. I'm going to keep messing with it and see if I can get it to work.

--- sketch.js
+++ sketch.js
@@ -5,10 +5,12 @@ let enterName;
 let bg;
 let nameBoxLength = 200;
 let option;
+let scene;
 
 function preload(){
   schoolFrontBg = loadImage("images/school_front_bg.png")
   dayHall = loadImage("images/dayHall.png");
+  nextArrow = loadImage("images/nextArrow.png")
   fancy = loadFont("images/font3.otf")
   normalF = loadFont("images/font0.otf")
 }
@@ -18,6 +20,7 @@ function setup(){
   //bg = new Backgrounds();
   //bg.setting(1);
   option = 0;
+  scene = 1;
   textFont(fancy,70);
   background(schoolFrontBg);
   text("DROPS IN THE WIND", 10,200);
@@ -29,9 +32,7 @@ function setup(){
 
 
 function draw(){
-  // if(option == 1){
-  //     background(schoolFrontBg);
-  //   }
+  storyTime();
 }
 
 function information(){
@@ -69,17 +70,65 @@ function inputName(){
   name = createInput("What is your name?"); //creating the textbox
   enterName = createButton('enter'); //creating the button
   name.position(width/2 - nameBoxLength/2,height/2);
-  enterName.position(width/2 ,height/2 + 50);
+  enterName.position(width/2,height/2 + 50);
   name.size(nameBoxLength);
-  enterName.mousePressed(name.changed(greeting)); //when the button is clicked, run the greeting function on the name
-  name.changed(greeting); //when the name is entered (changed), run the greeting function
+  enterName.mousePressed(name.changed(storyTime)); //when the button is clicked, run the greeting function on the name
+  name.changed(storyTime); //when the name is entered (changed), run the greeting function
 }
 
-function greeting(){
-  text("Hello there " + name.value() + "! Welcome to Tandon High School!")
-  storyTime();
+// function startStory(){
+//   scene = 1;
+// }
+
+// function greeting(){
+//   text("Hello there " + name.value() + "! Welcome to Tandon High School!")
+//   storyTime();
+// }
+
+function textBox(chara){
+  fill(255,214,174);
+  stroke(178,58,30);
+  strokeWeight(3);
+  rect(22.5,425.5,748,156,5); //bigger rectangle dimensions
+  if (chara = 1){ //christina text box
+    fill(253,187,199);
+    stroke(178,58,30);
+    rect(53,396,233,39,5);
+    fill(0);
+    textSize(20);
+    noStroke();
+    text("Christina",(233 - textWidth("Christina"))/2,423);
+    stroke(253,187,199);
+  }
 }
+
+// function nextScene(){
+//   image(nextArrow,730,550);
+//   if(mouseIsPressed == true){
+//     //if(mouseX >= 730 && mouseX <= 760 && mouseY >= 550 && mouseY <= 575){
+//       scene = scene + 1;
+//     //}
+//   }
+// }
 
 function storyTime(){
+  if (scene = 1){
+    enterName.remove();
+    name.remove();
+    background(dayHall);
+    textBox(1);
+    text("Hi " + name.value() + "! Welcome to Tandon High!", 30,460);
+    text("I’m Christina, the class president. I’ll show you around.", 30,490);
+    //nextScene();
+  } else if (scene = 2){
+    textBox(1);
+    text("Here, we have the-", 30,460);
+  }
+}
 
+function mouseClicked(){
+  if (scene >= 1){
+  scene = scene +1;
+  print(scene);
+}
 }
